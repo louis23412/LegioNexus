4750a503290f7e92faf567d86ac6dc6078e73d27
Louis <l.v.n.23412@gmail.com>
2026-04-14 21:25:38 +0200

    auto start event runner with each new conversation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
+import os from 'os';
 import path from 'path';
 import { io } from 'socket.io-client';
 import { Worker } from 'worker_threads';
 
 import { startConversation } from './agents/runAgent.js';
+
+const getLocalIP = () => {
+    const interfaces = os.networkInterfaces();
+    let ipAddress;
+
+    Object.keys(interfaces).forEach((ifaceName) => {
+        interfaces[ifaceName].forEach((iface) => {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                ipAddress = iface.address;
+            }
+        });
+    });
+
+    return ipAddress;
+}
 
 const spawnDedicatedHttpServer = (ip, port) => {
     const httpWorker = new Worker(path.join(import.meta.dirname, 'api', 'http_worker.js'), { workerData : { ip, port } });
@@ -17,16 +33,24 @@ const spawnDedicatedHttpServer = (ip, port) => {
 };
 
 const main = async (conversationId, userPrompt, userAlias) => {
-    const socket = await io.connect('http://localhost:3002/chat', { reconnection: true });
+    const ipAddress = getLocalIP();
+    const port = 3005;
+
+    spawnDedicatedHttpServer(ipAddress, port);
+
+    const socket = await io.connect(`http://${ipAddress}:${port}/chat`, { reconnection: true });
+
     await socket.on('connect', async () => {
+        console.log(`Event server running : http://${ipAddress}:${port}/chat\n`);
+
         const result = await startConversation(conversationId, userPrompt, userAlias, socket);
         if (!result.success) console.log(result.error);
     });
 };
 
+// -------------------------------------
 const conversationId = 'test_conversation_1'
 const userPrompt = 'How many items are in the test array?';
 const userAlias = 'test-user1';
 
-spawnDedicatedHttpServer('localhost', 3002);
 main(conversationId, userPrompt, userAlias).catch(console.error);
